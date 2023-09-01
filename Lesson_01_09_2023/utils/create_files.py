789b61843bbcc55b7a86c1979e611ec97c45e536
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-01 23:59:06 +0300

    feat(01_09_23): In the file "create_files.py " added logic

--- Lesson_01_09_2023/utils/create_files.py
+++ Lesson_01_09_2023/utils/create_files.py
@@ -1,5 +1,20 @@
-def main() -> None:
+def create_folders(folders: tuple) -> None:
     pass
+
+
+def create_src(path_with_name: str) -> None:
+    pass
+
+
+def create_tests(path: str, name: str) -> None:
+    pass
+
+
+def main() -> None:
+    create_folders(("../src/", "../tests/"))
+    for number in range(1, 26):
+        create_src(f"../src/Task_{number}.py")
+        create_tests("../tests/", f"test_Task_{number}.py")
 
 
 if __name__ == "__main__":
