a6a81065a843b461fc9f66af0cf1fa632a157362
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-03 19:10:59 +0300

    feat(01_09_23): Added solution and tests for the 9th task

--- Lesson_01_09_2023/src/Task_9.py
+++ Lesson_01_09_2023/src/Task_9.py
@@ -1,10 +1,18 @@
-def solution() -> None:
-	pass
+def solution(row_1: int, row_2: int, row_3: int) -> bool:
+	matrix = [[], [], []]
+	for i in range(3):
+		matrix[0].append(int(f"{row_1}"[i]))
+		matrix[1].append(int(f"{row_2}"[i]))
+		matrix[2].append(int(f"{row_3}"[i]))
+	raw_sum = tuple(sum(i) for i in matrix)
+	column_sum = tuple(sum(i) for i in zip(*matrix))
+	diagonal_sum = (sum(matrix[i][i] for i in range(3)), sum(matrix[i][2 - i] for i in range(3)))
+	return raw_sum[1:] == diagonal_sum if raw_sum == column_sum else False
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	return "YES" if solution(*map(int, args)) else "NO"
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input(), input(), input()))
--- Lesson_01_09_2023/tests/test_Task_9.py
+++ Lesson_01_09_2023/tests/test_Task_9.py
@@ -3,11 +3,17 @@ import src.Task_9
 
 
 class Task9TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_9.solution(), None)
+	def test_solution_1(self):
+		self.assertFalse(src.Task_9.solution(123, 456, 789))
 
-	def test_main(self):
-		self.assertEqual(src.Task_9.main(), None)
+	def test_solution_2(self):
+		self.assertTrue(src.Task_9.solution(111, 111, 111))
+
+	def test_main_1(self):
+		self.assertEqual(src.Task_9.main("123", "456", "789"), "NO")
+
+	def test_main_2(self):
+		self.assertEqual(src.Task_9.main("111", "111", "111"), "YES")
 
 
 if __name__ == "__main__":
