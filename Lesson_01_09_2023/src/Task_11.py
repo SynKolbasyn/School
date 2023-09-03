1b9593fa9c9664a597fbf09bebe7d7a63c0abc08
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-03 19:43:33 +0300

    feat(01_09_23): Added solution and tests for the 11th task

--- Lesson_01_09_2023/src/Task_11.py
+++ Lesson_01_09_2023/src/Task_11.py
@@ -1,10 +1,17 @@
-def solution() -> None:
-	pass
+def solution(number: int) -> str:
+	if 10 < number < 20:
+		return "Столов"
+	end = number % 10
+	if end == 1:
+		return "Стол"
+	if 1 < end < 5:
+		return "Стола"
+	return "Столов"
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	return solution(int(args[0]))
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_11.py
+++ Lesson_01_09_2023/tests/test_Task_11.py
@@ -3,11 +3,35 @@ import src.Task_11
 
 
 class Task11TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_11.solution(), None)
+	def test_solution_1(self):
+		self.assertEqual(src.Task_11.solution(1), "Стол")
 
-	def test_main(self):
-		self.assertEqual(src.Task_11.main(), None)
+	def test_solution_2(self):
+		self.assertEqual(src.Task_11.solution(2), "Стола")
+
+	def test_solution_3(self):
+		self.assertEqual(src.Task_11.solution(5), "Столов")
+
+	def test_solution_4(self):
+		self.assertEqual(src.Task_11.solution(11), "Столов")
+
+	def test_solution_5(self):
+		self.assertEqual(src.Task_11.solution(12), "Столов")
+
+	def test_main_1(self):
+		self.assertEqual(src.Task_11.main("1"), "Стол")
+
+	def test_main_2(self):
+		self.assertEqual(src.Task_11.main("2"), "Стола")
+
+	def test_main_3(self):
+		self.assertEqual(src.Task_11.main("5"), "Столов")
+
+	def test_main_4(self):
+		self.assertEqual(src.Task_11.main("11"), "Столов")
+
+	def test_main_5(self):
+		self.assertEqual(src.Task_11.main("12"), "Столов")
 
 
 if __name__ == "__main__":
