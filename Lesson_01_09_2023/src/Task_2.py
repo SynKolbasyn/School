197397b85c89ca27aee43b2b9f2e70e800a8165b
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-03 14:40:23 +0300

    feat(01_09_23): Added solution and tests for the 2nd task

--- Lesson_01_09_2023/src/Task_2.py
+++ Lesson_01_09_2023/src/Task_2.py
@@ -1,10 +1,14 @@
-def solution() -> None:
-	pass
+def solution(s: int, v1: int, v2: int) -> float:
+	return int(s / (v2 - v1))
 
 
-def main(*args, **kwargs) -> None:
-	return solution()
+def main(*args, **kwargs) -> float:
+	if not kwargs:
+		if len(args) == 1:
+			return solution(*tuple(map(int, args[0].split())))
+		return solution(*tuple(map(int, args)))
+	return solution(s=int(kwargs["s"]), v1=int(kwargs["v1"]), v2=int(kwargs["v2"]))
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_2.py
+++ Lesson_01_09_2023/tests/test_Task_2.py
@@ -4,10 +4,16 @@ import src.Task_2
 
 class Task2TestCase(unittest.TestCase):
 	def test_solution(self):
-		self.assertEqual(src.Task_2.solution(), None)
+		self.assertEqual(src.Task_2.solution(10, 3, 5), 5.0)
 
-	def test_main(self):
-		self.assertEqual(src.Task_2.main(), None)
+	def test_main_args_0(self):
+		self.assertEqual(src.Task_2.main("10 3 5"), 5.0)
+
+	def test_main_args(self):
+		self.assertEqual(src.Task_2.main("10", "3", "5"), 5.0)
+
+	def test_main_kwargs(self):
+		self.assertEqual(src.Task_2.main(s="10", v1="3", v2="5"), 5.0)
 
 
 if __name__ == "__main__":
