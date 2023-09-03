5bb29521ec869348770b6ac8bda929f243d5b41c
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-03 15:45:06 +0300

    feat(01_09_23): Added solution and tests for the 5th task

--- Lesson_01_09_2023/src/Task_5.py
+++ Lesson_01_09_2023/src/Task_5.py
@@ -1,10 +1,17 @@
-def solution() -> None:
-	pass
+def solution(n: int) -> str:
+	if n % len(f"{n}") != 0:
+		return "IMPOSSIBLE"
+	result = ""
+	while len(result) != n:
+		result += f"{n}"
+	return result
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	if not kwargs:
+		return solution(int(args[0]))
+	return solution(int(kwargs["n"]))
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_5.py
+++ Lesson_01_09_2023/tests/test_Task_5.py
@@ -4,10 +4,16 @@ import src.Task_5
 
 class Task5TestCase(unittest.TestCase):
 	def test_solution(self):
-		self.assertEqual(src.Task_5.solution(), None)
+		self.assertEqual(src.Task_5.solution(10), "1010101010")
+
+	def test_solution_impossible(self):
+		self.assertEqual(src.Task_5.solution(11), "IMPOSSIBLE")
 
 	def test_main(self):
-		self.assertEqual(src.Task_5.main(), None)
+		self.assertEqual(src.Task_5.main("10"), "1010101010")
+
+	def test_main_impossible(self):
+		self.assertEqual(src.Task_5.main("11"), "IMPOSSIBLE")
 
 
 if __name__ == "__main__":
