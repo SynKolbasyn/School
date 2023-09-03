6c7dd8fab2d30608691e88d7cc128850a7b8dffe
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-03 15:01:00 +0300

    feat(01_09_23): Added solution and tests for the 3rd task

--- Lesson_01_09_2023/src/Task_3.py
+++ Lesson_01_09_2023/src/Task_3.py
@@ -1,10 +1,14 @@
-def solution() -> None:
-	pass
+def solution(n: int) -> tuple[tuple, tuple]:
+	list_1 = tuple(n for _ in range(n))
+	list_2 = tuple(n for _ in f"{n}")
+	return (list_1, list_2) if len(list_1) < len(list_2) else (list_2, list_1)
 
 
-def main(*args, **kwargs) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	if not kwargs:
+		return "{}\n{}".format(*solution(int(args[0])))
+	return "{}\n{}".format(*solution(int(kwargs["n"])))
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_3.py
+++ Lesson_01_09_2023/tests/test_Task_3.py
@@ -4,10 +4,10 @@ import src.Task_3
 
 class Task3TestCase(unittest.TestCase):
 	def test_solution(self):
-		self.assertEqual(src.Task_3.solution(), None)
+		self.assertEqual(src.Task_3.solution(10), ((10, 10), (10, 10, 10, 10, 10, 10, 10, 10, 10, 10)))
 
 	def test_main(self):
-		self.assertEqual(src.Task_3.main(), None)
+		self.assertEqual(src.Task_3.main("10"), "(10, 10)\n(10, 10, 10, 10, 10, 10, 10, 10, 10, 10)")
 
 
 if __name__ == "__main__":
