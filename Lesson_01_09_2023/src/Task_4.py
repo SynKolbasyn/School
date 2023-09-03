154b7950b518d2f07ff6681f15514df70f3305a1
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-03 15:34:06 +0300

    feat(01_09_23): Added solution and tests for the 4th task

--- Lesson_01_09_2023/src/Task_4.py
+++ Lesson_01_09_2023/src/Task_4.py
@@ -1,10 +1,22 @@
-def solution() -> None:
-	pass
+def solution(number: int) -> list[int, ...]:
+	answer = []
+	for i in range(10):
+		old_num = f"{number - i}{i}"
+		if len(old_num) < 3:
+			continue
+		answer.append(int(old_num))
+	return answer
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	answer = ""
+	for i in solution(int(args[0])):
+		if len(answer) == 0:
+			answer = f"{i}"
+			continue
+		answer = f"{answer}\n{i}"
+	return answer
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_4.py
+++ Lesson_01_09_2023/tests/test_Task_4.py
@@ -3,11 +3,17 @@ import src.Task_4
 
 
 class Task4TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_4.solution(), None)
+	def test_solution_1(self):
+		self.assertEqual(src.Task_4.solution(10), [100])
 
-	def test_main(self):
-		self.assertEqual(src.Task_4.main(), None)
+	def test_solution_2(self):
+		self.assertEqual(src.Task_4.solution(99), [990, 981, 972, 963, 954, 945, 936, 927, 918, 909])
+
+	def test_main_1(self):
+		self.assertEqual(src.Task_4.main("10"), "100")
+
+	def test_main_2(self):
+		self.assertEqual(src.Task_4.main("99"), "990\n981\n972\n963\n954\n945\n936\n927\n918\n909")
 
 
 if __name__ == "__main__":
