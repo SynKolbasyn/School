a06a6af6a96ee07c06b2160d9ef2aa988a690e0b
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-03 20:01:59 +0300

    feat(01_09_23): Added solution and tests for the 12th task

--- Lesson_01_09_2023/src/Task_12.py
+++ Lesson_01_09_2023/src/Task_12.py
@@ -1,10 +1,14 @@
-def solution() -> None:
-	pass
+def solution(road_1: str, road_2: str) -> int:
+	road_1_rate = 3 if road_1.lower() == "cat" else (2 if road_1.lower() == "pigeon" else 1)
+	road_2_rate = 3 if road_2.lower() == "cat" else (2 if road_2.lower() == "pigeon" else 1)
+	return 1 if road_1_rate >= road_2_rate else 2
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> int:
+	if len(args) == 1:
+		return solution(*args[0].split())
+	return solution(args[0], args[1])
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_12.py
+++ Lesson_01_09_2023/tests/test_Task_12.py
@@ -3,11 +3,86 @@ import src.Task_12
 
 
 class Task12TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_12.solution(), None)
+	def test_solution_1(self):
+		self.assertEqual(src.Task_12.solution("cat", "cat"), 1)
 
-	def test_main(self):
-		self.assertEqual(src.Task_12.main(), None)
+	def test_solution_2(self):
+		self.assertEqual(src.Task_12.solution("pigeon", "pigeon"), 1)
+
+	def test_solution_3(self):
+		self.assertEqual(src.Task_12.solution("dog", "dog"), 1)
+
+	def test_solution_4(self):
+		self.assertEqual(src.Task_12.solution("cat", "pigeon"), 1)
+
+	def test_solution_5(self):
+		self.assertEqual(src.Task_12.solution("pigeon", "dog"), 1)
+
+	def test_solution_6(self):
+		self.assertEqual(src.Task_12.solution("pigeon", "cat"), 2)
+
+	def test_solution_7(self):
+		self.assertEqual(src.Task_12.solution("dog", "pigeon"), 2)
+
+	def test_solution_8(self):
+		self.assertEqual(src.Task_12.solution("cat", "dog"), 1)
+
+	def test_solution_9(self):
+		self.assertEqual(src.Task_12.solution("dog", "cat"), 2)
+
+	def test_main_arg_1(self):
+		self.assertEqual(src.Task_12.main("cat cat"), 1)
+
+	def test_main_arg_2(self):
+		self.assertEqual(src.Task_12.main("pigeon pigeon"), 1)
+
+	def test_main_arg_3(self):
+		self.assertEqual(src.Task_12.main("dog dog"), 1)
+
+	def test_main_arg_4(self):
+		self.assertEqual(src.Task_12.main("cat pigeon"), 1)
+
+	def test_main_arg_5(self):
+		self.assertEqual(src.Task_12.main("pigeon dog"), 1)
+
+	def test_main_arg_6(self):
+		self.assertEqual(src.Task_12.main("pigeon cat"), 2)
+
+	def test_main_arg_7(self):
+		self.assertEqual(src.Task_12.main("dog pigeon"), 2)
+
+	def test_main_arg_8(self):
+		self.assertEqual(src.Task_12.main("cat dog"), 1)
+
+	def test_main_arg_9(self):
+		self.assertEqual(src.Task_12.main("dog cat"), 2)
+
+	def test_main_args_1(self):
+		self.assertEqual(src.Task_12.main("cat", "cat"), 1)
+
+	def test_main_args_2(self):
+		self.assertEqual(src.Task_12.main("pigeon", "pigeon"), 1)
+
+	def test_main_args_3(self):
+		self.assertEqual(src.Task_12.main("dog", "dog"), 1)
+
+	def test_main_args_4(self):
+		self.assertEqual(src.Task_12.main("cat", "pigeon"), 1)
+
+	def test_main_args_5(self):
+		self.assertEqual(src.Task_12.main("pigeon", "dog"), 1)
+
+	def test_main_args_6(self):
+		self.assertEqual(src.Task_12.main("pigeon", "cat"), 2)
+
+	def test_main_args_7(self):
+		self.assertEqual(src.Task_12.main("dog", "pigeon"), 2)
+
+	def test_main_args_8(self):
+		self.assertEqual(src.Task_12.main("cat", "dog"), 1)
+
+	def test_main_args_9(self):
+		self.assertEqual(src.Task_12.main("dog", "cat"), 2)
 
 
 if __name__ == "__main__":
