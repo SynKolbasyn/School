1473a4cad71f74cb5bc4891179d61f56056429b1
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-03 19:30:13 +0300

    feat(01_09_23): Added solution and tests for the 10th task

--- Lesson_01_09_2023/src/Task_10.py
+++ Lesson_01_09_2023/src/Task_10.py
@@ -1,10 +1,18 @@
-def solution() -> None:
-	pass
+def solution(l1: int, r1: int, l2: int, r2: int) -> bool:
+	return r1 >= l2 and l1 <= r2
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	if not kwargs:
+		if len(args) == 1:
+			return "YES" if solution(*map(int, args[0].split())) else "NO"
+		return "YES" if solution(*map(int, args)) else "NO"
+	return "YES" if solution(
+		l1=int(kwargs["l1"]),
+		r1=int(kwargs["r1"]),
+		l2=int(kwargs["l2"]),
+		r2=int(kwargs["r2"])) else "NO"
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_10.py
+++ Lesson_01_09_2023/tests/test_Task_10.py
@@ -3,11 +3,65 @@ import src.Task_10
 
 
 class Task10TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_10.solution(), None)
+	def test_solution_1(self):
+		self.assertTrue(src.Task_10.solution(0, 5, 2, 7))
 
-	def test_main(self):
-		self.assertEqual(src.Task_10.main(), None)
+	def test_solution_2(self):
+		self.assertTrue(src.Task_10.solution(0, 0, 0, 0))
+
+	def test_solution_3(self):
+		self.assertTrue(src.Task_10.solution(-5, 0, 0, 5))
+
+	def test_solution_4(self):
+		self.assertFalse(src.Task_10.solution(-5, -2, 2, 5))
+
+	def test_solution_5(self):
+		self.assertFalse(src.Task_10.solution(2, 5, -5, -2))
+
+	def test_main_arg_1(self):
+		self.assertEqual(src.Task_10.main("0 5 2 7"), "YES")
+
+	def test_main_arg_2(self):
+		self.assertEqual(src.Task_10.main("0 0 0 0"), "YES")
+
+	def test_main_arg_3(self):
+		self.assertEqual(src.Task_10.main("-5 0 0 5"), "YES")
+
+	def test_main_arg_4(self):
+		self.assertEqual(src.Task_10.main("-5 -2 2 5"), "NO")
+
+	def test_main_arg_5(self):
+		self.assertEqual(src.Task_10.main("2 5 -5 -2"), "NO")
+
+	def test_main_args_1(self):
+		self.assertEqual(src.Task_10.main("0 5 2 7"), "YES")
+
+	def test_main_args_2(self):
+		self.assertEqual(src.Task_10.main("0", "0", "0", "0"), "YES")
+
+	def test_main_args_3(self):
+		self.assertEqual(src.Task_10.main("-5", "0", "0", "5"), "YES")
+
+	def test_main_args_4(self):
+		self.assertEqual(src.Task_10.main("-5", "-2", "2", "5"), "NO")
+
+	def test_main_args_5(self):
+		self.assertEqual(src.Task_10.main("2", "5", "-5", "-2"), "NO")
+
+	def test_main_kwargs_1(self):
+		self.assertEqual(src.Task_10.main("0 5 2 7"), "YES")
+
+	def test_main_kwargs_2(self):
+		self.assertEqual(src.Task_10.main(l1="0", r1="0", l2="0", r2="0"), "YES")
+
+	def test_main_kwargs_3(self):
+		self.assertEqual(src.Task_10.main(l1="-5", r1="0", l2="0", r2="5"), "YES")
+
+	def test_main_kwargs_4(self):
+		self.assertEqual(src.Task_10.main(l1="-5", r1="-2", l2="2", r2="5"), "NO")
+
+	def test_main_kwargs_5(self):
+		self.assertEqual(src.Task_10.main(l1="2", r1="5", l2="-5", r2="-2"), "NO")
 
 
 if __name__ == "__main__":
