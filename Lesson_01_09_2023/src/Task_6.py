8f05612a14c8a3dee8ab4850cc2503ecba76b3be
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-03 17:12:25 +0300

    feat(01_09_23): Added solution and tests for the 6th task

--- Lesson_01_09_2023/src/Task_6.py
+++ Lesson_01_09_2023/src/Task_6.py
@@ -1,10 +1,15 @@
-def solution() -> None:
-	pass
+def solution(n: int, x: int) -> int:
+	override = x % n
+	return min(override, n - override)
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> int:
+	if not kwargs:
+		if len(args) == 1:
+			return solution(*map(int, args[0].split()))
+		return solution(*map(int, args))
+	return solution(n=int(kwargs["n"]), x=int(kwargs["x"]))
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_6.py
+++ Lesson_01_09_2023/tests/test_Task_6.py
@@ -3,11 +3,29 @@ import src.Task_6
 
 
 class Task6TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_6.solution(), None)
+	def test_solution_1(self):
+		self.assertEqual(src.Task_6.solution(10, 3), 3)
 
-	def test_main(self):
-		self.assertEqual(src.Task_6.main(), None)
+	def test_solution_2(self):
+		self.assertEqual(src.Task_6.solution(10, 7), 3)
+
+	def test_main_arg_1(self):
+		self.assertEqual(src.Task_6.main("10 3"), 3)
+
+	def test_main_arg_2(self):
+		self.assertEqual(src.Task_6.main("10 7"), 3)
+
+	def test_main_args_1(self):
+		self.assertEqual(src.Task_6.main("10", "3"), 3)
+
+	def test_main_args_2(self):
+		self.assertEqual(src.Task_6.main("10", "7"), 3)
+
+	def test_main_kwargs_1(self):
+		self.assertEqual(src.Task_6.main(n="10", x="3"), 3)
+
+	def test_main_kwargs_2(self):
+		self.assertEqual(src.Task_6.main(n="10", x="7"), 3)
 
 
 if __name__ == "__main__":
