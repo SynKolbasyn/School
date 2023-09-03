dab163fe07ddd9246c317ac618fb1d3e434ece1d
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-03 18:31:20 +0300

    feat(01_09_23): Added solution and tests for the 8th task

--- Lesson_01_09_2023/src/Task_8.py
+++ Lesson_01_09_2023/src/Task_8.py
@@ -1,10 +1,24 @@
-def solution() -> None:
-	pass
+def solution(a: int, b: int, c: int, n: int) -> tuple[float, float, float]:
+	cycle_time = a + b + c + b
+	cycles_count = n / cycle_time
+	red = (a + b) * cycles_count
+	yellow = (b + b) * cycles_count
+	green = c * cycles_count
+	return red, yellow, green
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	if not kwargs:
+		if len(args) == 1:
+			return "Red: {}\nYellow: {}\nGreen: {}".format(*solution(*map(int, args[0].split())))
+		return "Red: {}\nYellow: {}\nGreen: {}".format(*solution(*map(int, args)))
+	return "Red: {}\nYellow: {}\nGreen: {}".format(*solution(
+		a=int(kwargs["a"]),
+		b=int(kwargs["b"]),
+		c=int(kwargs["c"]),
+		n=int(kwargs["n"])
+	))
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_8.py
+++ Lesson_01_09_2023/tests/test_Task_8.py
@@ -4,10 +4,16 @@ import src.Task_8
 
 class Task8TestCase(unittest.TestCase):
 	def test_solution(self):
-		self.assertEqual(src.Task_8.solution(), None)
+		self.assertEqual(src.Task_8.solution(5, 2, 5, 14), (7, 4, 5))
 
-	def test_main(self):
-		self.assertEqual(src.Task_8.main(), None)
+	def test_main_arg(self):
+		self.assertEqual(src.Task_8.main("5 2 5 14"), "Red: 7.0\nYellow: 4.0\nGreen: 5.0")
+
+	def test_main_args(self):
+		self.assertEqual(src.Task_8.main("5", "2", "5", "14"), "Red: 7.0\nYellow: 4.0\nGreen: 5.0")
+
+	def test_main_kwargs(self):
+		self.assertEqual(src.Task_8.main(a="5", b="2", c="5", n="14"), "Red: 7.0\nYellow: 4.0\nGreen: 5.0")
 
 
 if __name__ == "__main__":
