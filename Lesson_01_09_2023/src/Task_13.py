f0b2bdcaf0d264d52f46807c273a80179a121664
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-04 21:48:46 +0300

    feat(01_09_23): Added solution and tests for the 13th task

--- Lesson_01_09_2023/src/Task_13.py
+++ Lesson_01_09_2023/src/Task_13.py
@@ -1,10 +1,20 @@
-def solution() -> None:
-	pass
+def solution(x: int, y: int) -> int:
+	if x < 0 and y < 0:
+		return max(x + y, x ** y)
+	if x < 0 == y:
+		return max(x + y, x ** y, int(f"{x}{y}"))
+	if y < 0 == x:
+		return max(x + y, y ** x, int(f"{y}{x}"))
+	return max(x + y, x ** y, y ** x, int(f"{x}{y}"), int(f"{y}{x}"))
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> int:
+	if kwargs:
+		return solution(x=int(kwargs["x"]), y=int(kwargs["y"]))
+	if len(args) == 1:
+		return solution(*map(int, args[0].split()))
+	return solution(*map(int, args))
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_13.py
+++ Lesson_01_09_2023/tests/test_Task_13.py
@@ -3,11 +3,77 @@ import src.Task_13
 
 
 class Task13TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_13.solution(), None)
+	def test_solution_1(self):
+		self.assertEqual(src.Task_13.solution(0, 0), 1)
 
-	def test_main(self):
-		self.assertEqual(src.Task_13.main(), None)
+	def test_solution_2(self):
+		self.assertEqual(src.Task_13.solution(-1, 0), 1)
+
+	def test_solution_3(self):
+		self.assertEqual(src.Task_13.solution(-1, -1), -1)
+
+	def test_solution_4(self):
+		self.assertEqual(src.Task_13.solution(2, 2), 22)
+
+	def test_solution_5(self):
+		self.assertEqual(src.Task_13.solution(2, 3), 32)
+
+	def test_solution_6(self):
+		self.assertEqual(src.Task_13.solution(3, 2), 32)
+
+	def test_main_arg_1(self):
+		self.assertEqual(src.Task_13.main("0 0"), 1)
+
+	def test_main_arg_2(self):
+		self.assertEqual(src.Task_13.main("-1 0"), 1)
+
+	def test_main_arg_3(self):
+		self.assertEqual(src.Task_13.main("-1 -1"), -1)
+
+	def test_main_arg_4(self):
+		self.assertEqual(src.Task_13.main("2 2"), 22)
+
+	def test_main_arg_5(self):
+		self.assertEqual(src.Task_13.main("2 3"), 32)
+
+	def test_main_arg_6(self):
+		self.assertEqual(src.Task_13.main("3 2"), 32)
+
+	def test_main_args_1(self):
+		self.assertEqual(src.Task_13.main("0", "0"), 1)
+
+	def test_main_args_2(self):
+		self.assertEqual(src.Task_13.main("-1", "0"), 1)
+
+	def test_main_args_3(self):
+		self.assertEqual(src.Task_13.main("-1", "-1"), -1)
+
+	def test_main_args_4(self):
+		self.assertEqual(src.Task_13.main("2", "2"), 22)
+
+	def test_main_args_5(self):
+		self.assertEqual(src.Task_13.main("2", "3"), 32)
+
+	def test_main_args_6(self):
+		self.assertEqual(src.Task_13.main("3", "2"), 32)
+
+	def test_main_kwargs_1(self):
+		self.assertEqual(src.Task_13.main(x="0", y="0"), 1)
+
+	def test_main_kwargs_2(self):
+		self.assertEqual(src.Task_13.main(x="-1", y="0"), 1)
+
+	def test_main_kwargs_3(self):
+		self.assertEqual(src.Task_13.main(x="-1", y="-1"), -1)
+
+	def test_main_kwargs_4(self):
+		self.assertEqual(src.Task_13.main(x="2", y="2"), 22)
+
+	def test_main_kwargs_5(self):
+		self.assertEqual(src.Task_13.main(x="2", y="3"), 32)
+
+	def test_main_kwargs_6(self):
+		self.assertEqual(src.Task_13.main(x="3", y="2"), 32)
 
 
 if __name__ == "__main__":
