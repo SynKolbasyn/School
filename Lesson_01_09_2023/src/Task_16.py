ab23e0dd94051fcf780691947b7e108861ccbefe
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-05 20:46:10 +0300

    feat(01_09_23): Added solution and tests for the 16th task

--- Lesson_01_09_2023/src/Task_16.py
+++ Lesson_01_09_2023/src/Task_16.py
@@ -1,10 +1,21 @@
-def solution() -> None:
-	pass
+def solution(a: int, b: int, r: int, u: int) -> tuple[int, str]:
+	horizontal = min(a - r, r)
+	vertical = min(b - u, u)
+	steps = horizontal + vertical
+	way = "R" * r + "U" * u + ("L" if horizontal == r else "R") * horizontal + ("D" if vertical == u else "U") * vertical
+	return steps, way
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	if kwargs:
+		steps, way = solution(a=int(kwargs["a"]), b=int(kwargs["b"]), r=int(kwargs["r"]), u=int(kwargs["u"]))
+		return f"{steps}\n{way}"
+	if len(args) == 1:
+		steps, way = solution(*map(int, args[0].split()))
+		return f"{steps}\n{way}"
+	steps, way = solution(*map(int, args))
+	return f"{steps}\n{way}"
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_16.py
+++ Lesson_01_09_2023/tests/test_Task_16.py
@@ -4,10 +4,16 @@ import src.Task_16
 
 class Task16TestCase(unittest.TestCase):
 	def test_solution(self):
-		self.assertEqual(src.Task_16.solution(), None)
+		self.assertEqual(src.Task_16.solution(10, 10, 7, 8), (5, "RRRRRRRUUUUUUUURRRUU"))
 
-	def test_main(self):
-		self.assertEqual(src.Task_16.main(), None)
+	def test_main_arg(self):
+		self.assertEqual(src.Task_16.main("10 10 7 8"), "5\nRRRRRRRUUUUUUUURRRUU")
+
+	def test_main_args(self):
+		self.assertEqual(src.Task_16.main("10", "10", "7", "8"), "5\nRRRRRRRUUUUUUUURRRUU")
+
+	def test_main_kwargs(self):
+		self.assertEqual(src.Task_16.main(a="10", b="10", r="7", u="8"), "5\nRRRRRRRUUUUUUUURRRUU")
 
 
 if __name__ == "__main__":
