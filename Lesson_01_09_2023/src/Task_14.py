8243370c45647401a6ddf651a9ea051c46b61c1c
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-05 20:12:45 +0300

    feat(01_09_23): Added solution and tests for the 14th task

--- Lesson_01_09_2023/src/Task_14.py
+++ Lesson_01_09_2023/src/Task_14.py
@@ -1,10 +1,27 @@
-def solution() -> None:
-	pass
+def solution(array: tuple[int, ...]) -> list[int]:
+	n = len(array)
+	result = []
+	for i in array:
+		if i % n == 0:
+			result.append(i)
+	return result
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def make_str(array: list[int, ...]) -> str:
+	result = ""
+	for i in array:
+		if result == "":
+			result = f"{i}"
+			continue
+		result = f"{result}\n{i}"
+	return result
+
+
+def main(*args: str, **kwargs: str) -> str:
+	if len(args) == 1:
+		return make_str(solution(tuple(map(int, args[0].split()))))
+	return make_str(solution(tuple(map(int, args))))
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_14.py
+++ Lesson_01_09_2023/tests/test_Task_14.py
@@ -3,11 +3,23 @@ import src.Task_14
 
 
 class Task14TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_14.solution(), None)
+	def test_solution_1(self):
+		self.assertEqual(src.Task_14.solution((123, 321, 442)), [123, 321])
 
-	def test_main(self):
-		self.assertEqual(src.Task_14.main(), None)
+	def test_solution_2(self):
+		self.assertEqual(src.Task_14.solution((5483, 1836, 8126, 8253)), [1836])
+
+	def test_main_arg_1(self):
+		self.assertEqual(src.Task_14.main("123 321 442"), "123\n321")
+
+	def test_main_arg_2(self):
+		self.assertEqual(src.Task_14.main("5483 1836 8126 8253"), "1836")
+
+	def test_main_args_1(self):
+		self.assertEqual(src.Task_14.main("123", "321", "442"), "123\n321")
+
+	def test_main_args_2(self):
+		self.assertEqual(src.Task_14.main("5483", "1836", "8126", "8253"), "1836")
 
 
 if __name__ == "__main__":
