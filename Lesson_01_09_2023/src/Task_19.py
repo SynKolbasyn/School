f719258407886f5e2946d8884d7d815501084b5f
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-05 21:20:42 +0300

    feat(01_09_23): Added solution and tests for the 19th task

--- Lesson_01_09_2023/src/Task_19.py
+++ Lesson_01_09_2023/src/Task_19.py
@@ -1,10 +1,17 @@
-def solution() -> None:
-	pass
+def solution(stations: tuple[str, ...]) -> int:
+	past_stations = []
+	for i in stations:
+		if i in past_stations:
+			return len(past_stations)
+		past_stations.append(i)
+	return len(past_stations)
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> int:
+	if len(args) == 1:
+		return solution(tuple(args[0].split()))
+	return solution(args)
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_19.py
+++ Lesson_01_09_2023/tests/test_Task_19.py
@@ -4,10 +4,13 @@ import src.Task_19
 
 class Task19TestCase(unittest.TestCase):
 	def test_solution(self):
-		self.assertEqual(src.Task_19.solution(), None)
+		self.assertEqual(src.Task_19.solution(("A", "B", "C", "A")), 3)
 
-	def test_main(self):
-		self.assertEqual(src.Task_19.main(), None)
+	def test_main_arg(self):
+		self.assertEqual(src.Task_19.main("A B C A"), 3)
+
+	def test_main_args(self):
+		self.assertEqual(src.Task_19.main("A", "B", "C", "A"), 3)
 
 
 if __name__ == "__main__":
