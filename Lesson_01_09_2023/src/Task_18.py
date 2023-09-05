0d298ffe8dcd463ac6315ba3032b7352ec53159a
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-05 21:02:46 +0300

    feat(01_09_23): Added solution and tests for the 18th task

--- Lesson_01_09_2023/src/Task_18.py
+++ Lesson_01_09_2023/src/Task_18.py
@@ -1,10 +1,19 @@
-def solution() -> None:
-	pass
+def solution(n: int) -> str:
+	result = ""
+	old_result = ""
+	for i in range(1, n + 1):
+		result += f"{i}"
+		if len(result) > n:
+			return old_result
+		old_result = result
+	return result
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	if kwargs:
+		return solution(n=int(kwargs["n"]))
+	return solution(int(args[0]))
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_18.py
+++ Lesson_01_09_2023/tests/test_Task_18.py
@@ -3,11 +3,32 @@ import src.Task_18
 
 
 class Task18TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_18.solution(), None)
+	def test_solution_1(self):
+		self.assertEqual(src.Task_18.solution(10), "123456789")
 
-	def test_main(self):
-		self.assertEqual(src.Task_18.main(), None)
+	def test_solution_2(self):
+		self.assertEqual(src.Task_18.solution(3), "123")
+
+	def test_solution_3(self):
+		self.assertEqual(src.Task_18.solution(11), "12345678910")
+
+	def test_main_arg_1(self):
+		self.assertEqual(src.Task_18.main("10"), "123456789")
+
+	def test_main_arg_2(self):
+		self.assertEqual(src.Task_18.main("3"), "123")
+
+	def test_main_arg_3(self):
+		self.assertEqual(src.Task_18.main("11"), "12345678910")
+
+	def test_main_kwargs_1(self):
+		self.assertEqual(src.Task_18.main(n="10"), "123456789")
+
+	def test_main_kwargs_2(self):
+		self.assertEqual(src.Task_18.main(n="3"), "123")
+
+	def test_main_kwargs_3(self):
+		self.assertEqual(src.Task_18.main(n="11"), "12345678910")
 
 
 if __name__ == "__main__":
