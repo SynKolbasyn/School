1427c60c05de96d433dcf39c265dff55f393dedb
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-05 21:29:28 +0300

    feat(01_09_23): Added solution and tests for the 20th task

--- Lesson_01_09_2023/src/Task_20.py
+++ Lesson_01_09_2023/src/Task_20.py
@@ -1,10 +1,17 @@
-def solution() -> None:
-	pass
+def solution(string: str) -> str:
+	result = ""
+	old_char = ""
+	for i in string:
+		if (i != old_char) and (old_char != ""):
+			result += " "
+		result += i
+		old_char = i
+	return result
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	return solution(args[0])
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_20.py
+++ Lesson_01_09_2023/tests/test_Task_20.py
@@ -3,11 +3,17 @@ import src.Task_20
 
 
 class Task20TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_20.solution(), None)
+	def test_solution_1(self):
+		self.assertEqual(src.Task_20.solution("aaabbbccc"), "aaa bbb ccc")
 
-	def test_main(self):
-		self.assertEqual(src.Task_20.main(), None)
+	def test_solution_2(self):
+		self.assertEqual(src.Task_20.solution("abc"), "a b c")
+
+	def test_main_arg_1(self):
+		self.assertEqual(src.Task_20.main("aaabbbccc"), "aaa bbb ccc")
+
+	def test_main_arg_2(self):
+		self.assertEqual(src.Task_20.main("abc"), "a b c")
 
 
 if __name__ == "__main__":
