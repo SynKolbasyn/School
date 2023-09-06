6e8b61386c297c0d97edd609eedc276c1fcbf792
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-06 15:43:33 +0300

    feat(01_09_23): Added solution and tests for the 25th task

--- Lesson_01_09_2023/src/Task_25.py
+++ Lesson_01_09_2023/src/Task_25.py
@@ -1,10 +1,16 @@
-def solution() -> None:
-	pass
+def solution(string: str) -> bool:
+	old_char = "a"
+	string = string.lower()
+	for i in string:
+		if i < old_char:
+			return False
+		old_char = i
+	return True
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	return "YES" if solution(args[0]) else "NO"
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_25.py
+++ Lesson_01_09_2023/tests/test_Task_25.py
@@ -3,11 +3,29 @@ import src.Task_25
 
 
 class Task25TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_25.solution(), None)
+	def test_solution_1(self):
+		self.assertTrue(src.Task_25.solution("ABCDEF"))
 
-	def test_main(self):
-		self.assertEqual(src.Task_25.main(), None)
+	def test_solution_2(self):
+		self.assertFalse(src.Task_25.solution("FEDCBA"))
+
+	def test_solution_3(self):
+		self.assertTrue(src.Task_25.solution("AbCdEf"))
+
+	def test_solution_4(self):
+		self.assertFalse(src.Task_25.solution("FeDcBa"))
+
+	def test_main_arg_1(self):
+		self.assertEqual(src.Task_25.main("ABCDEF"), "YES")
+
+	def test_main_arg_2(self):
+		self.assertEqual(src.Task_25.main("FEDCBA"), "NO")
+
+	def test_main_arg_3(self):
+		self.assertEqual(src.Task_25.main("AbCdEf"), "YES")
+
+	def test_main_arg_4(self):
+		self.assertEqual(src.Task_25.main("FeDcBa"), "NO")
 
 
 if __name__ == "__main__":
