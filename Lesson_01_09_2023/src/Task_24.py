7ff37dae463a9d52aebf2f2621c3e084c0cf9ec5
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-06 15:35:41 +0300

    feat(01_09_23): Added solution and tests for the 24th task

--- Lesson_01_09_2023/src/Task_24.py
+++ Lesson_01_09_2023/src/Task_24.py
@@ -1,10 +1,17 @@
-def solution() -> None:
-	pass
+def solution(string: str, n: int) -> str:
+	result = ""
+	iterations = (len(string) - n) // (n + 1)
+	for i in range(iterations):
+		result += string[(i + 1) * (n + 1) - 1]
+	return result
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	if len(args) == 1:
+		string, n = args[0].split()
+		return solution(string, int(n))
+	return solution(args[0], int(args[1]))
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_24.py
+++ Lesson_01_09_2023/tests/test_Task_24.py
@@ -3,11 +3,23 @@ import src.Task_24
 
 
 class Task24TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_24.solution(), None)
+	def test_solution_1(self):
+		self.assertEqual(src.Task_24.solution("ABCDEFG", 1), "BDF")
 
-	def test_main(self):
-		self.assertEqual(src.Task_24.main(), None)
+	def test_solution_2(self):
+		self.assertEqual(src.Task_24.solution("ABCDEFGH", 2), "CF")
+
+	def test_main_arg_1(self):
+		self.assertEqual(src.Task_24.main("ABCDEFG 1"), "BDF")
+
+	def test_main_arg_2(self):
+		self.assertEqual(src.Task_24.main("ABCDEFGH 2"), "CF")
+
+	def test_main_args_1(self):
+		self.assertEqual(src.Task_24.main("ABCDEFG", "1"), "BDF")
+
+	def test_main_args_2(self):
+		self.assertEqual(src.Task_24.main("ABCDEFGH", "2"), "CF")
 
 
 if __name__ == "__main__":
