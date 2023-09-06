8880f80559f159ecfc85aa53b3a9d89b3fc4fc18
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-06 08:22:27 +0300

    feat(01_09_23): Added solution and tests for the 22th task

--- Lesson_01_09_2023/src/Task_22.py
+++ Lesson_01_09_2023/src/Task_22.py
@@ -1,10 +1,13 @@
-def solution() -> None:
-	pass
+def solution(games: tuple[tuple[int, int], ...]) -> bool:
+	for a, v in games:
+		if a > v:
+			return True
+	return False
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	return "YES" if solution(tuple(tuple(map(int, i.split())) for i in args)) else "NO"
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input(), input()))
--- Lesson_01_09_2023/tests/test_Task_22.py
+++ Lesson_01_09_2023/tests/test_Task_22.py
@@ -3,11 +3,23 @@ import src.Task_22
 
 
 class Task22TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_22.solution(), None)
+	def test_solution_1(self):
+		self.assertTrue(src.Task_22.solution(((1, 2), (2, 1))))
 
-	def test_main(self):
-		self.assertEqual(src.Task_22.main(), None)
+	def test_solution_2(self):
+		self.assertFalse(src.Task_22.solution(((1, 2), (1, 2))))
+
+	def test_solution_3(self):
+		self.assertTrue(src.Task_22.solution(((2, 1), (2, 1))))
+
+	def test_main_args_1(self):
+		self.assertEqual(src.Task_22.main("1 2", "2 1"), "YES")
+
+	def test_main_args_2(self):
+		self.assertEqual(src.Task_22.main("1 2", "1 2"), "NO")
+
+	def test_main_args_3(self):
+		self.assertEqual(src.Task_22.main("2 1", "2 1"), "YES")
 
 
 if __name__ == "__main__":
