34767387600b1e0b702a7148ccc338760ad04ab5
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2023-09-06 15:01:41 +0300

    feat(01_09_23): Added solution and tests for the 23th task

--- Lesson_01_09_2023/src/Task_23.py
+++ Lesson_01_09_2023/src/Task_23.py
@@ -1,10 +1,19 @@
-def solution() -> None:
-	pass
+def solution(string: str) -> str:
+	vasya = string.count("Вася")
+	alice = string.count("Алиса")
+	i = string.count("Я")
+	vinner = max(vasya, alice, i)
+	if vinner == vasya:
+		return "Вася"
+	if vinner == alice:
+		return "Алиса"
+	if vinner == i:
+		return "Соня"
 
 
-def main(*args: str, **kwargs: str) -> None:
-	return solution()
+def main(*args: str, **kwargs: str) -> str:
+	return solution(args[0])
 
 
 if __name__ == "__main__":
-	print(main())
+	print(main(input()))
--- Lesson_01_09_2023/tests/test_Task_23.py
+++ Lesson_01_09_2023/tests/test_Task_23.py
@@ -3,11 +3,17 @@ import src.Task_23
 
 
 class Task23TestCase(unittest.TestCase):
-	def test_solution(self):
-		self.assertEqual(src.Task_23.solution(), None)
+	def test_solution_1(self):
+		self.assertEqual(src.Task_23.solution("ВасяВасяАлисаАлисаАлисаЯ"), "Алиса")
 
-	def test_main(self):
-		self.assertEqual(src.Task_23.main(), None)
+	def test_solution_2(self):
+		self.assertEqual(src.Task_23.solution("ВасяВасяАлисаАлисаАлисаЯЯЯЯЯ"), "Соня")
+
+	def test_main_arg_1(self):
+		self.assertEqual(src.Task_23.main("ВасяВасяАлисаАлисаАлисаЯ"), "Алиса")
+
+	def test_main_arg_2(self):
+		self.assertEqual(src.Task_23.main("ВасяВасяАлисаАлисаАлисаЯЯЯЯЯ"), "Соня")
 
 
 if __name__ == "__main__":
